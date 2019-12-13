fe536571d64a855dd6134b06299f691717023690
satoru <sato.kapu@gmail.com>
2019-12-13 18:49:08 +0900

    second commit

---
+++ chapter_2/2.2.cpp
@@ -0,0 +1,12 @@
+//M-x shell
+//M-!
+//Hello, World!と出力する
+
+#include "./std_lib_facilities.h"
+
+int main()
+{
+  cout << "Hello, Programming!\n" << "Here we go!\n";
+  keep_window_open();
+  return 0;
+}
